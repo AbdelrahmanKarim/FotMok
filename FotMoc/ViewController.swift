c0d6edd03c5dfea33908c9d808ede7220ec7176e
me <me>
2026-05-19 09:22:57 -0700

    Initial Commit

---
+++ FotMoc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FotMoc
+//
+//  Created by abdelrahman karim on 19/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
