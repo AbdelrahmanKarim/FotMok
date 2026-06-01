a6dca53f444dfe53417104442b58a12768dca709
me <me>
2026-06-01 07:10:24 -0700

    feat(add): sharable components

---
+++ FotMoc/presentation/design_system/components/statsGridCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  statsGridCollectionViewCell.swift
+//  FotMoc
+//
+//  Created by abdelrahman karim on 30/05/2026.
+//
+
+import UIKit
+
+class statsGridCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var statTitleLabel: UILabel!
+    @IBOutlet weak var statValueLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
