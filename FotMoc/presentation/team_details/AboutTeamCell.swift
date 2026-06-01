e5956cd976d7188b264239b51fcb4a3af1b2eed2
me <me>
2026-06-01 07:12:22 -0700

    feat(add): team details view controller & components

---
+++ FotMoc/presentation/team_details/AboutTeamCell.swift
@@ -0,0 +1,17 @@
+//
+//  AboutTeamCell.swift
+//  FotMoc
+//
+//  Created by abdelrahman karim on 31/05/2026.
+//
+
+import UIKit
+
+class AboutTeamCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ FotMoc/presentation/team_details/KeyPlayerCell.swift
@@ -0,0 +1,17 @@
+//
+//  KeyPlayerCell.swift
+//  FotMoc
+//
+//  Created by abdelrahman karim on 31/05/2026.
+//
+
+import UIKit
+
+class KeyPlayerCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
