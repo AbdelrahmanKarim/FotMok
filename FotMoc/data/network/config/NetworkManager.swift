ea94c321cb41f6a5d0a54ab4c3aaac6b19f53e7e
me <me>
2026-06-03 05:54:38 -0700

    feat(add): network manager config

---
+++ FotMoc/data/network/config/NetworkManager.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkManager.swift
+//  FotMoc
+//
+//  Created by abdelrahman karim on 02/06/2026.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkManager {
+    
+    private let baseURL = Secrets.baseURL
+    private let apiKey = Secrets.apiKey
+    
+    func fetch<T: Decodable>(sport: String, parameters: [String: Any]) async throws -> ResultDTO<T> {
+        let url = "\(baseURL)/\(sport)/"
+        var finalParameters = parameters
+        finalParameters["APIkey"] = apiKey
+        
+       
+        return try await AF.request(url, method: .get, parameters: finalParameters)
+            .validate()
+            .serializingDecodable(ResultDTO<T>.self)
+            .value
+    }
+}
+enum Secrets {
+    
+    static var apiKey: String {
+        guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "API_KEY") as? String else {
+            fatalError("API_KEY not found in Info.plist")
+        }
+        return apiKey
+    }
+    
+    static var baseURL: String {
+        guard let baseURL = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else {
+            fatalError("BASE_URL not found in Info.plist")
+        }
+        return baseURL
+    }
+}
