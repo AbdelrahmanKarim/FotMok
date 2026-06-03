bb17d16b69bd9bd4ed185eadd714754b78893138
alaaayman <alaaaymanfouad@gmail.com>
2026-06-03 13:17:43 -0700

    implemented getLeagues and searchLeagues in datasource

--- FotMoc/data/datasource/league/remote/LeagueRemoteDataSource.swift
+++ FotMoc/data/datasource/league/remote/LeagueRemoteDataSource.swift
@@ -7,7 +7,8 @@
 
 
 import Foundation
-
+import RxSwift
 protocol LeagueRemoteDataSource {
-    
+    func getLeagues(sport: SportType) async throws -> [LeagueDTO]
+    func searchLeagues(sport: SportType, query: String) -> Observable<[LeagueDTO]>
 }
--- FotMoc/data/datasource/league/remote/LeagueRemoteDataSourceImpl.swift
+++ FotMoc/data/datasource/league/remote/LeagueRemoteDataSourceImpl.swift
@@ -7,7 +7,7 @@
 
 
 import Foundation
-
+import RxSwift
 class LeagueRemoteDataSourceImpl: LeagueRemoteDataSource {
     private let service: LeagueService
         
@@ -15,4 +15,42 @@ class LeagueRemoteDataSourceImpl: LeagueRemoteDataSource {
             self.service = service
         }
     
+    func getLeagues(sport: SportType) async throws -> [LeagueDTO] {
+            
+            let dtoResult = try await service.fetchLeagues(sport: sport.rawValue)
+        
+            guard let leagueDTOs = dtoResult.result else {
+                return []
+            }
+            return leagueDTOs
+        }
+    
+    func searchLeagues(sport: SportType, query: String) -> Observable<[LeagueDTO]> {
+        
+            let cleanQuery = query.trimmingCharacters(in: .whitespacesAndNewlines)
+            if cleanQuery.isEmpty {
+                return Observable.just([])
+            }
+            
+            return Observable.create { [weak self] observer in
+                let task = Task {
+                    do {
+                        let dtos = try await self?.getLeagues(sport: sport) ?? []
+                        observer.onNext(dtos)
+                        observer.onCompleted()
+                    } catch {
+                        observer.onError(error)
+                    }
+                }
+                return Disposables.create {
+                    task.cancel()
+                }
+            }
+           
+            .map { dtos in
+                dtos.filter { dto in
+                    dto.leagueName.lowercased().contains(cleanQuery.lowercased())
+                }
+            }
+        }
 }
