18e824ae68bf999c0de89638fda7b2d550d7d620
me <me>
2026-06-04 10:26:57 -0700

    feat(add): get league table standings

--- FotMoc/data/datasource/league/remote/LeagueRemoteDataSourceImpl.swift
+++ FotMoc/data/datasource/league/remote/LeagueRemoteDataSourceImpl.swift
@@ -8,11 +8,21 @@
 
 import Foundation
 
+
 class LeagueRemoteDataSourceImpl: LeagueRemoteDataSource {
     private let service: LeagueService
         
-        init(service: LeagueService) {
-            self.service = service
-        }
+    init(service: LeagueService) {
+        self.service = service
+    }
     
+    
+        
+    func getLeagueTableStandings(sport: String, leagueId: String) async throws -> [Total] {
+        do {
+            let res = try await service.fetchStandings(sport: sport, leagueId: leagueId)
+            guard res.success == 1, let result = res.result?.total else { throw AppException.noData }
+            return result
+        } catch { throw AppException.map(error) }
+    }
 }
--- FotMoc/data/repository/LeagueRepositoryImpl.swift
+++ FotMoc/data/repository/LeagueRepositoryImpl.swift
@@ -5,36 +5,25 @@
 //  Created by abdelrahman karim on 02/06/2026.
 //
 class LeagueRepositoryImpl: LeagueRepository {
-    
     private let remoteDataSource: LeagueRemoteDataSource
     private let localDataSource: LeagueLocalDataSource
+    private let sportProvider: CurrentSportProvider
         
-    init(remoteDataSource: LeagueRemoteDataSource, localDataSource: LeagueLocalDataSource) {
-            self.remoteDataSource = remoteDataSource
-            self.localDataSource = localDataSource
+    init(remoteDataSource: LeagueRemoteDataSource, localDataSource: LeagueLocalDataSource, sportProvider: CurrentSportProvider) {
+        self.remoteDataSource = remoteDataSource
+        self.localDataSource = localDataSource
+        self.sportProvider = sportProvider
     }
     
-    func getFavouriteLeagues() async throws -> [League] {
-        fatalError()
-    }
+    func getFavouriteLeagues() async throws -> [League] { fatalError() }
+    func getLeagues(sport: SportType) async throws -> [League] { fatalError() }
+    func saveFavouriteLeague(league: League) async throws { fatalError() }
+    func searchLeagues(query: String) async throws -> [League] { fatalError() }
     
-    func getLeagueDetails(leagueId: String) async throws -> League {
-        fatalError()
-    }
-    
-    func getLeagues(sport: SportType) async throws -> [League] {
-        fatalError()
-    }
-    
+        
     func getLeagueTableStandings(leagueId: String) async throws -> [StandingRow] {
-        fatalError()
-    }
-    
-    func saveFavouriteLeague(league: League) async throws {
-        fatalError()
-    }
-    
-    func searchLeagues(query: String) async throws -> [League] {
-        fatalError()
+        let sport = sportProvider.selectedSport
+        let dtos = try await remoteDataSource.getLeagueTableStandings(sport: sport.rawValue, leagueId: leagueId)
+        return dtos.map { $0.toEntity(sport: sport) }
     }
 }
